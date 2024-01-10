e24864c7ee1ff0f09aa83303e0754a3c5cfd7c7d
rebecacoelho <rebecasantoscoelho@gmail.com>
2024-01-10 20:32:05 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,12 @@
+import { type AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { type AccountModel } from '../../../../domain/models/account'
+import { type AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne({ ...accountData })
+    return { ...accountData, id: result.insertedId.toString() }
+  }
+}
