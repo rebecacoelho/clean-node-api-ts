d1f6ce069c74ab5cfd01bfa0d2a338d386f15c7e
rebecacoelho <rebecasantoscoelho@gmail.com>
2024-01-13 12:46:22 -0300

    feat: ensure SignUp route returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import setupRoutes from './routes'
 
 const app = express()
 setupMiddlewares(app)
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+setupRoutes(app)
 
 export default app
