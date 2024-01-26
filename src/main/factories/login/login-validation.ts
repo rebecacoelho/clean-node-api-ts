668f5e2f1c5c1775072a98d65d60f83c90b5cd30
rebecacoelho <rebecasantoscoelho@gmail.com>
2024-01-25 23:55:43 -0300

    refactor: move Validation to protocols folder

--- src/main/factories/login/login-validation.ts
+++ src/main/factories/login/login-validation.ts
@@ -1,6 +1,6 @@
 import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
 import { RequiredFieldValidator } from '../../../presentation/helpers/validators/required-field-validator'
-import { type Validation } from '../../../presentation/helpers/validators/validation'
+import { type Validation } from '../../../presentation/protocols/validation'
 import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
 import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
 
