01b7acbd26ff5f47dba784f4e35c1e3c3f25a6b2
rebecacoelho <rebecasantoscoelho@gmail.com>
2024-01-28 15:16:07 -0300

    refactor: remove duplicated code

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -2,11 +2,7 @@ import bcrypt from 'bcrypt'
 import { type Hasher } from '../../../data/protocols/criptography/hasher'
 
 export class BcryptAdapter implements Hasher {
-  private readonly salt: number
-
-  constructor (salt: number) { // itens expecificos de cada bibilioteca sao passadas no constructor para nao alterar os protocolos genricos
-    this.salt = salt
-  }
+  constructor (private readonly salt: number) {} // itens expecificos de cada bibilioteca sao passadas no constructor para nao alterar os protocolos genricos
 
   async hash (value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt)
