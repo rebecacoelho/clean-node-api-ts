17de967e786591c573d07ecdcb8fc45ff8c02b7c
rebecacoelho <rebecasantoscoelho@gmail.com>
2024-01-31 10:44:37 -0300

    build: ajust env data

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://rebecasantoscoelho:Sq7Hbho8B0L43cFv@clean-api-node.fbjogta.mongodb.net/clean-api-node?retryWrites=true&w=majority ',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api-ts',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj67O==5H'
 }
